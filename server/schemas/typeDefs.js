43e681c27a05ab02602f318b8e7713ac36d5a336
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-10-11 17:21:52 -0500

    added Book, User, and Auth types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,28 @@ const { gql } = require('apollo-server-express')
 
 
 const typeDefs = gql`
+  type User {
+    _id:ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
   input BookInput {
     authors: [String]
     description: String!
